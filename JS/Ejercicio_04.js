ee44b14d6ea82a782b554307378cae68fed435d8
Esther González Peralta <230297@utxicotepec.edu.mx>
2024-10-03 14:24:25 -0600

    Práctica06
    Se agregaron los apartados 6, 7, 8, 9 y 10 al archivo ejercicio_04 de la carpte JS

--- JS/Ejercicio_04.js
+++ JS/Ejercicio_04.js
@@ -114,3 +114,93 @@ console.log("¿Y para las iregulares?")
 let numeroFilas= matrizIrregular.length
 for(let i=0; i<numeroFilas; i++)
     console.log(`La longitud de la fila  ${[i]} es = ${[matrizIrregular[i].length]}`)
+
+    console.log("%c5.- Agregar un nuevo elemento a un arreglo (PUSH)", style_console);
+
+    let estudiantes =["Angel Refugio", "Idai Vargas", "Daniel Bravo", "Esther González", "Ailton Artiaga"]
+    console.log("Los elementos actuales del arreglo son:")
+    console.table(estudiantes)
+    console.log("Agregamos  a un nuevo estudiante llamdo: Abril Guzmán")
+    estudiantes.push("Abril Guzmán")
+    console.log("Despúes de agregar los elementos del arreglo son:")
+    console.table(estudiantes)
+    console.log("¿Qué pasa con los mixtos?")
+    console.log("El arregloMixto actualmente tiene los siguientes elementos:")
+    console.table(arregloMixto);
+    console.log("Agregamos la palabra: \"Hola\", como nuevo elemento")
+    arregloMixto.push("Hola");
+    console.log("Y también agregamos el número -312881787448415.23597518864 siendo este un BigInt");
+    arregloMixto.push(BigInt(-31288178744841522555.23597518864));
+    console.log("Despúes de estas dos operaciones, el arreglo queda con los isguientes elementos:");
+    console.table(arregloMixto);
+
+    console.log("%c6.- Agregar un nuevo elemento a un arreglo (UNISHIFT) en la posicion inicial", style_console);
+    console.table(estudiantes)
+    console.log("Ahora agregaremos a Abishai Flores, al comienzo del arreglo.")
+    estudiantes.unshift("M");
+    console.log("La lista actual es:")
+    console.table(estudiantes)
+
+
+    console.log("%c7.- Eliminar el elemento de un arreglo en la última posición (POP)", style_console);
+    console.log("El arreglo tiwnw los siguientes elementos:")
+    console.table(estudiantes)
+    console.log("Para este caso, eliminaremos Abril Guzmán en la última posición")
+    estudiantes.pop();
+    console.log("Despúes de eliminar el arreglo quedo de la siguiente manera:")
+    console.table(estudiantes);
+
+    console.log("%c8.- Eliminar el elemento de un arreglo en la última posición (SHIFT)", style_console);
+    console.log("El arreglo tiene los siguientes elementos:")
+    console.table(estudiantes)
+    console.log("Para este caso, eliminaremos Abishai Flores en la primera posición")
+    estudiantes.shift();
+    console.log("Despúes de eliminar el arreglo quedo de la siguiente manera:")
+    console.table(estudiantes);
+
+
+    console.log("%c9.- Modificar un arreglo en un arreglo nuevo con poición definidas (SPLICE)", style_console);
+    console.log("El arreglo original tiene los elementos:")
+    console.table(estudiantes)
+    console.log("Dividir el arreglo en uno nuevo eliminando ciertos elementos en base a su posición")
+    //cuando la función splice recibe un solo parámetro eliminará los elementos de esta posición en adelante
+    estudiantes.splice(2)
+    console.table(estudiantes);
+    //cuando la fución splice recibe 2 parametr9*os elimina todos los elemenos que no estén en ese rango 
+
+    estudiantes.push("Zyanta Zacatenco");
+    estudiantes.push("ZTania Ibarra");
+    estudiantes.push("Juvenal Viveros");
+    estudiantes.push("Marcos Rios");
+    estudiantes.push("Jonathan Baldemar");
+    console.log("Se han agregado 5 nuevos estudiantes, por lo que el arreglo es:")
+    console.table(estudiantes);
+    console.log("Ahora ya tenemos los elementos suficientes para aplicar el método splice con dos parámetros qeu seran 3,5")
+    estudiantes.splice(3,5)
+    console.log("Resultado en:");
+    console.table(estudiantes);
+
+    //splice sirve tambien para insertar elementos en posiciones específicas, ahora necesitamos insertar a: "Antonio Ocpaco" entre "Angel Refugio" "Idai Vargas"
+
+    console.log("Ahora vamos a insertar a \"Antonio Ocpaco\" entre los elementos de la posicion 0 y 1")
+    estudiantes.splice(1,0,"Antonio Ocpaco")
+    console.log("Resultado en:");
+    console.table(estudiantes);
+
+    // Tambien splice sirve para reeemplazar lementos por otros, en este caso reemplazaremos a "Angel Rufino" por "Brandon Leon"
+    console.log("Ahora vamos a reemplazar a \"Angel Rufino\" entre los elementos de la posicion \"Brandon Leon\"")
+    estudiantes.splice(0,1,"Brandon Leon")
+    console.log("Resultado en:");
+    console.table(estudiantes);
+
+    console.log("%c10.- Métodos para la manipulación de Arreglos INMUTABLES", style_console);
+    let signosZodiacales=["Aries","Tauro","Géminis", "Cáncer", "Leo", "Virgo", "Libra", "Escorpio", "Sagitario","Capricornio","Acuario", "piscis"]
+
+    //Destructuración de Arreglos
+    let[signo1, , signo3, , , ,signo7, , , , ,]=signosZodiacales;
+    console.log(`El primer signo Zodiacal es: ${signo1}`)
+    console.log(`El segundo signo Zodiacal es: ${signo3}`)
+    console.log(`El septimo signo Zodiacal es: ${signo7}`)
+
+    //Congelamos el arreglo volviendolo INMUTABLE;
+    Object.freeze(signosZodiacales);

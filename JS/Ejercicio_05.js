ec8ff9f58af6ee1cadc1043375fae02e2a30e0e3
Esther González Peralta <230297@utxicotepec.edu.mx>
2024-10-07 20:58:01 -0600

    Practica 07
    Se corrigió el código del ejercicio 05 de la carpeta JS

--- JS/Ejercicio_05.js
+++ JS/Ejercicio_05.js
@@ -1,10 +1,8 @@
 //Repaso de ciclos y sentencias de constrol
 
-//Estilización de los mensajes de salida 
-
-const bg="linear-gradient(11deg, rgba(199, 0, 36, 1) 0%, rgba(255, 191, 126, 1) 33%, rgba(253, 129, 226, 1)86%)";
-const style_console=`background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold;`
-
+//Estilización de los mensajes de salida
+const bg = "linear-gradient(11deg, rgba(0,90,36,1) 0%, rgba(0,90,26,1) 33%, rgba(0,90,26,1) 80%)";
+const style_console = `background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold`
 //Personalizacion de las salidas de consola
 
 console.warn("Práctica 06:Arreglos en Java Script")
@@ -16,32 +14,42 @@ let fechaActual=new Date();
 console.log(`Hola, la fecha de hoy es: ${fechaActual.toString()}`);
 
 //y si la necesitamos en formato local?
-const fechaLocalMX=fechaActual.toLocaleString(`es-MX`,
-{
-        weekday:'long',
-        year:'null',
-        month:'long',
-        day:'numeric',
-        hour:'numeric',
-        minute:'numeric',
-        second:'numeric',
-        hour12: false
-        
-        
-    }
-);
+const fechaLocalMX= fechaActual.toLocaleString('es-MX', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false
+});
 
-console.log(`en formato local (México): ${fechaLocalMX}`);
+console.log(`en formato local (México): ${fechaLocalMX}`)
 
-//si es antes de las 12 y saluda con un buenos dias 
+//Si es antes de las doce saluda con buenos días
 
 if(fechaActual.getHours()<12)
-    console.log(`Buenos dias, hoy es:${fechaLocalMX}`);
+    console.log(`Buenos días, hoy es ${fechaLocalMX}`)
 
-//Existe un extensor de la sentencia if (Si) que es else (en caso contrario)
+//Existe un extensor de la sentencia if(Sí) que es else (En Caso contrario)
 if(fechaActual.getMonth()<=6)
-    console.log(`Estas en la primera mitad del año`);
+    console.log(`Estas en la primera mitad del año`)
 else
-console.log(`Estas en la segunda mitad del año`);
+    console.log(`Estas en la segunda mitad del año.`)
 
-//que pas si la validacion tiene varias operaciones 
+
+//que pasa si la validación tiene varias operaciones 
+const anio= fechaActual.getFullYear()
+let inicioPrimavera= new Date(anio, 2, 21);
+let inicioVerano= new Date(anio, 5, 21);
+let inicioOtonio= new Date(anio, 8, 23);
+let inicioInvierno= new Date(anio, 11, 21);
+
+let horarioVerano= false;
+
+if(fechaActual>= inicioPrimavera || fechaActual< inicioVerano)
+{
+    console.log("Estamos en PRIMAVERA...")
+
+}
